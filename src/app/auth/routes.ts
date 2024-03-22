37a378fcae838c704c91ab14a82327c63b280f04
loushiklk <loushikkumargiri@gmail.com>
2024-03-22 12:51:59 +0530

    remove all auth route except github login route

--- src/app/auth/routes.ts
+++ src/app/auth/routes.ts
@@ -1,17 +1,5 @@
 import { Router } from "express";
-import passport from "passport";
-import envConfig from "../../configs/env.config";
 import Controllers from "./controllers";
-
-import {
-  validateChangePassword,
-  validateEmailLogin,
-  validateEmailRegistration,
-  validateEmailVerify,
-  validateForgotPassword,
-  validateForgotPasswordOTPVerify,
-  validateResendVerificationCode,
-} from "./validations";
 
 export default class AuthRouter {
   public router: Router;
@@ -24,28 +12,28 @@ export default class AuthRouter {
   }
 
   public routes() {
-    this.router.post(
-      "/email-register",
-      validateEmailRegistration(),
-      this.controller.register.bind(this.controller)
-    );
-    this.router.post(
-      "/login",
-      validateEmailLogin(),
-      this.controller.userLogin.bind(this.controller)
-    );
-    this.router.get(
-      "/google/login",
-      passport.authenticate("google", { scope: ["profile", "email"] })
-    );
-    this.router.get(
-      "/google/callback",
-      passport.authenticate("google", {
-        scope: ["profile", "email"],
-        failureRedirect: envConfig().LoginFailedCallbackURL,
-      }),
-      this.controller.createUserToken.bind(this.controller)
-    );
+    // this.router.post(
+    //   "/email-register",
+    //   validateEmailRegistration(),
+    //   this.controller.register.bind(this.controller)
+    // );
+    // this.router.post(
+    //   "/login",
+    //   validateEmailLogin(),
+    //   this.controller.userLogin.bind(this.controller)
+    // );
+    // this.router.get(
+    //   "/google/login",
+    //   passport.authenticate("google", { scope: ["profile", "email"] })
+    // );
+    // this.router.get(
+    //   "/google/callback",
+    //   passport.authenticate("google", {
+    //     scope: ["profile", "email"],
+    //     failureRedirect: envConfig().LoginFailedCallbackURL,
+    //   }),
+    //   this.controller.createUserToken.bind(this.controller)
+    // );
 
     this.router.get(
       "/github/login",
@@ -61,31 +49,31 @@ export default class AuthRouter {
       "/generate-token",
       this.controller.createUserToken.bind(this.controller)
     );
-    this.router.post(
-      "/change-password",
-      validateChangePassword(),
-      this.controller.changePassword.bind(this.controller)
-    );
-    this.router.post(
-      "/resend-verification-code",
-      validateResendVerificationCode(),
-      this.controller.resendVerificationCode.bind(this.controller)
-    );
-    this.router.post(
-      "/verify",
-      validateEmailVerify(),
-      this.controller.verifyUser.bind(this.controller)
-    );
-    this.router.post(
-      "/forgot-password",
-      validateForgotPassword(),
-      this.controller.forgotPassword.bind(this.controller)
-    );
-    this.router.post(
-      "/forgot-password-verify",
-      validateForgotPasswordOTPVerify(),
-      this.controller.forgotPasswordVerify.bind(this.controller)
-    );
+    // this.router.post(
+    //   "/change-password",
+    //   validateChangePassword(),
+    //   this.controller.changePassword.bind(this.controller)
+    // );
+    // this.router.post(
+    //   "/resend-verification-code",
+    //   validateResendVerificationCode(),
+    //   this.controller.resendVerificationCode.bind(this.controller)
+    // );
+    // this.router.post(
+    //   "/verify",
+    //   validateEmailVerify(),
+    //   this.controller.verifyUser.bind(this.controller)
+    // );
+    // this.router.post(
+    //   "/forgot-password",
+    //   validateForgotPassword(),
+    //   this.controller.forgotPassword.bind(this.controller)
+    // );
+    // this.router.post(
+    //   "/forgot-password-verify",
+    //   validateForgotPasswordOTPVerify(),
+    //   this.controller.forgotPasswordVerify.bind(this.controller)
+    // );
     this.router.post("/logout", this.controller.logout.bind(this.controller));
   }
 }
